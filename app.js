56375163fe6706212899e5261292a9b9dabd1443
piltens <34446381+piltens@users.noreply.github.com>
2019-10-01 11:14:00 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: {exact: "environment" } }, audio: false };
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+
+};
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
